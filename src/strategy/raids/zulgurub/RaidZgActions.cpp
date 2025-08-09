ccf4f1e4c00ed4402559f3a1dcf4a0cb47ca4d83
crow <pengchengw@me.com>
2025-08-08 20:32:16 -0500

    movement testing

--- src/strategy/raids/zulgurub/RaidZgActions.cpp
+++ src/strategy/raids/zulgurub/RaidZgActions.cpp
@@ -4,21 +4,38 @@
 
 bool MandokirThreateningGazeFreezeAction::Execute(Event event)
 {
+LOG_INFO("bot", "Executing MandokirThreateningGazeFreezeAction for {}", bot->GetName());
+
     botAI->Reset();
+
+    LOG_INFO("bot", "StopMoving called for {}", GetName());
     bot->StopMoving();
-    
+
+    LOG_INFO("bot", "AttackStop called for {}", GetName());
     bot->AttackStop();
+
+    LOG_INFO("bot", "SetTarget called for {}", GetName());
     bot->SetTarget(ObjectGuid::Empty);
-    
+
     if (bot->IsNonMeleeSpellCast(false))
+    {
+        LOG_INFO("bot", "Interrupting non-melee spell for {}", GetName());
         bot->InterruptNonMeleeSpells(false);
-    
+    }
+
+    LOG_INFO("bot", "Clearing movement for {}", _owner->GetName());
     bot->GetMotionMaster()->Clear(false);
+
+    LOG_INFO("bot", "CombatStop called for {}", GetName());
     bot->CombatStop(true);
-    
+
+    LOG_INFO("bot", "Changing engine to non-combat for {}", GetName());
     botAI->ChangeEngine(BOT_STATE_NON_COMBAT);
 
+    LOG_INFO("bot", "Strategy changing to passive for {}", GetName());
     botAI->ChangeStrategy("+passive", BOT_STATE_NON_COMBAT);
+
+    LOG_INFO("bot", "Strategy changing to stay for {}", GetName());
     botAI->ChangeStrategy("+stay", BOT_STATE_NON_COMBAT);
     
     return true;
@@ -26,7 +43,9 @@ bool MandokirThreateningGazeFreezeAction::Execute(Event event)
 
 bool MandokirThreateningGazeResumeAction::Execute(Event event)
 {    
+    LOG_INFO("bot", "Strategy changing to active for {}", GetName());
     botAI->ChangeStrategy("-passive", BOT_STATE_NON_COMBAT);
+    LOG_INFO("bot", "Strategy changing to follow for {}", GetName());
     botAI->ChangeStrategy("+follow", BOT_STATE_NON_COMBAT);
     return true;
 }
