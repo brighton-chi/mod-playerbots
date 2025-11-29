337286bb8b8a6357f0753fae48ce1653899052b1
crow <pengchengw@me.com>
2025-11-29 01:31:24 -0600

    Update RaidZulAmanHelpers.h

--- src/strategy/raids/zulaman/RaidZulAmanHelpers.h
+++ src/strategy/raids/zulaman/RaidZulAmanHelpers.h
@@ -16,14 +16,30 @@ namespace ZulAmanHelpers
         SPELL_ELECTRICAL_STORM     = 43648,
 
         // Nalorakk <Bear Avatar>
+        SPELL_BEARFORM          = 42377
 
         // Jan'alai <Dragonhawk Avatar>
 
         // Halazzi <Lynx Avatar>
 
         // Hex Lord Malacrass
+        SPELL_LIFEBLOOM = 43421,
+        SPELL_AVENGING_WRATH = 43430,
+        SPELL_BLIND = 43433,
+        SPELL_MIND_CONTROL = 43550,
 
         // Zul'jin
+    };
+
+    enum ZulAmanNPCs
+    {
+        // Jan'alai <Dragonhawk Avatar>
+        NPC_AMANI_HATCHER           = 23818,
+        NPC_FIRE_BOMB               = 23920,
+
+        // Halazzi <Lynx Avatar>
+        // NPC_SPIRIT_OF_THE_LYNX      = 24143,
+        NPC_CORRUPTED_LIGHTNING_TOTEM = 24224,
     };
 
     namespace ZulAmanPositions
