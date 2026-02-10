2902d78b030397df69decfcc6e83a57bdf4f1b73
crow <pengchengw@me.com>
2026-02-10 01:04:49 -0600

    magtheridon stuff

--- src/Ai/Raid/Magtheridon/Multiplier/RaidMagtheridonMultipliers.cpp
+++ src/Ai/Raid/Magtheridon/Multiplier/RaidMagtheridonMultipliers.cpp
@@ -19,15 +19,17 @@ float MagtheridonUseManticronCubeMultiplier::GetValue(Action* action)
     if (!magtheridon)
         return 1.0f;
 
+    if (dynamic_cast<WipeAction*>(action))
+        return 1.0f;
+
     if (magtheridon->HasUnitState(UNIT_STATE_CASTING) &&
         magtheridon->FindCurrentSpellBySpellId(SPELL_BLAST_NOVA))
     {
         auto it = botToCubeAssignment.find(bot->GetGUID());
         if (it != botToCubeAssignment.end())
         {
-            if (dynamic_cast<WipeAction*>(action))
-                return 1.0f;
-            else if (!dynamic_cast<MagtheridonUseManticronCubeAction*>(action))
+            if (!dynamic_cast<MagtheridonUseManticronCubeAction*>(action) &&
+                !dynamic_cast<MagtheridonManageTimersAndAssignmentsAction*>(action))
                 return 0.0f;
         }
     }
@@ -42,7 +44,7 @@ float MagtheridonWaitToAttackMultiplier::GetValue(Action* action)
     if (!magtheridon || magtheridon->HasAura(SPELL_SHADOW_CAGE))
         return 1.0f;
 
-    if (botAI->IsMainTank(bot))
+    if (botAI->IsMainTank(bot) || botAI->IsHeal(bot))
         return 1.0f;
 
     const uint8 dpsWaitSeconds = 6;
@@ -51,7 +53,7 @@ float MagtheridonWaitToAttackMultiplier::GetValue(Action* action)
         (time(nullptr) - it->second) < dpsWaitSeconds)
     {
         if (dynamic_cast<AttackAction*>(action) ||
-            (!botAI->IsHeal(bot) && dynamic_cast<CastSpellAction*>(action)))
+            dynamic_cast<CastSpellAction*>(action))
             return 0.0f;
     }
 
@@ -70,6 +72,26 @@ float MagtheridonDisableOffTankAssistMultiplier::GetValue(Action* action)
     if ((botAI->IsAssistTankOfIndex(bot, 0) || botAI->IsAssistTankOfIndex(bot, 1)) &&
         dynamic_cast<TankAssistAction*>(action))
         return 0.0f;
+
+    return 1.0f;
+}
+
+float MagtheridonDisableMainTankMovementMultiplier::GetValue(Action* action)
+{
+    Unit* magtheridon = AI_VALUE2(Unit*, "find target", "magtheridon");
+    if (!magtheridon)
+        return 1.0f;
+
+    if (bot->GetVictim() == nullptr)
+        return 1.0f;
+
+    if (botAI->IsMainTank(bot))
+    {
+        if (dynamic_cast<TankAssistAction*>(action) ||
+            dynamic_cast<TankFaceAction*>(action) ||
+            dynamic_cast<AvoidAoeAction*>(action))
+            return 0.0f;
+    }
 
     return 1.0f;
 }
