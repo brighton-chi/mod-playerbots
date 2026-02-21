0e5c7325a39204e3e9d2816ade7a67f3344ccf4e
crow <pengchengw@me.com>
2026-02-21 01:53:58 -0600

    typo

--- src/Ai/Raid/TempestKeep/Util/RaidTempestKeepHelpers.h
+++ src/Ai/Raid/TempestKeep/Util/RaidTempestKeepHelpers.h
@@ -124,7 +124,7 @@ namespace TempestKeepHelpers
     int8 GetAlarCurrentLocationIndex(Unit* alar);
     void GetClosestPlatformAndGround(
         const Position& botPos, int8& closestPlatform, Position& ground);
-    std::pair<Unit*, Unit*> GetFirstTwoEmbersOfAlar(PlayerbotAI* bot);
+    std::pair<Unit*, Unit*> GetFirstTwoEmbersOfAlar(PlayerbotAI* botAI);
     Player* GetSecondEmberTank(PlayerbotAI* botAI);
 
     // Void Reaver
