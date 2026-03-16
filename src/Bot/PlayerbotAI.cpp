caab5056dec1840b486124b1a7cdb9a396955ce3
crow <pengchengw@me.com>
2026-03-16 17:42:35 -0500

    reimplement vertical speed exclusion for packets

--- src/Bot/PlayerbotAI.cpp
+++ src/Bot/PlayerbotAI.cpp
@@ -1281,8 +1281,8 @@ void PlayerbotAI::HandleBotOutgoingPacket(WorldPacket const& packet)
             }
             verticalSpeed = -verticalSpeed;
             // high vertical may result in stuck as bot can not handle gravity
-            if (verticalSpeed > 35.0f)
-                break;
+            /* if (verticalSpeed > 35.0f)
+                break;*/
             // stop casting
             InterruptSpell();
 
