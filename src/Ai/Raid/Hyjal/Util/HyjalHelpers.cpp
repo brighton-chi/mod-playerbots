c812814b36a9ae4515fafa8cab961b76128ae465
crow <pengchengw@me.com>
2026-09-07 14:31:12 -0500

    fix sonar false positive. maybe.

--- src/Ai/Raid/Hyjal/Util/HyjalHelpers.cpp
+++ src/Ai/Raid/Hyjal/Util/HyjalHelpers.cpp
@@ -404,8 +404,10 @@ Unit* GetLooseInfernal(PlayerbotAI* botAI)
     for (ObjectGuid const guid : GetInfernalGuids(botAI))
     {
         Unit* infernal = botAI->GetUnit(guid);
-        if (infernal && infernal->IsAlive() && infernal->GetVictim() != infernalTank)
-            return infernal;
+        if (!infernal || !infernal->IsAlive())
+            continue;
+
+        return infernal->GetVictim() != infernalTank ? infernal : nullptr;
     }
 
     return nullptr;
