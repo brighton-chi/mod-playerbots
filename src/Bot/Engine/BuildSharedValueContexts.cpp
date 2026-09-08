0c6c0f447a697099ce5feab7c8e3f7e181357e02
crow <pengchengw@me.com>
2026-09-08 00:20:36 -0500

    alphabetize value contexts

--- src/Bot/Engine/BuildSharedValueContexts.cpp
+++ src/Bot/Engine/BuildSharedValueContexts.cpp
@@ -18,7 +18,7 @@ void AiObjectContext::BuildSharedValueContexts(
     valueContexts.Add(new ValueContext());
     valueContexts.Add(new RaidHyjalValueContext());
     valueContexts.Add(new TbcDungeonMechValueContext());
+    valueContexts.Add(new TbcDungeonMgTValueContext());
     valueContexts.Add(new TbcDungeonUnderbogValueContext());
     valueContexts.Add(new WotlkDungeonGDValueContext());
-    valueContexts.Add(new TbcDungeonMgTValueContext());
 }
